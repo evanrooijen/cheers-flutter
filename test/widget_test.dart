5b5640a30d7e416eeba0fff592f88e0204e53d0b
Elmer van Rooijen <e.van.rooijen@hotmail.com>
2022-01-29 11:20:40 +0100

    test: fix test using auth mock

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,28 +6,21 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:cheers/auth/auth_repository.dart';
-import 'package:flutter/material.dart';
+import 'package:cheers/main.dart';
+import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:cheers/main.dart';
-
 void main() {
-  final AuthRepository authRepository = AuthRepository();
+  final AuthRepository authRepository =
+      AuthRepository(firebaseAuth: MockFirebaseAuth());
 
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Homepage Smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp(authRepository: authRepository));
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that our navigation buttons are present
+    expect(find.text('Login'), findsOneWidget);
+    expect(find.text('Register'), findsOneWidget);
+    expect(find.text('Home'), findsOneWidget);
   });
 }
